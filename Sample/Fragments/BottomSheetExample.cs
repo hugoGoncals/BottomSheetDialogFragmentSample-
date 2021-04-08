549a08a1d220e77bf80c4b70293fdf51389fd746
Hugo Gonçalves <hugo.goncalves@xpand-it.com>
2021-04-08 22:12:42 +0100

    First commit

---
+++ Sample/Fragments/BottomSheetExample.cs
@@ -0,0 +1,17 @@
+﻿using Android.OS;
+using Android.Views;
+using com.companyname.NavigationCodeLab;
+using Google.Android.Material.BottomSheet;
+
+namespace CAPMobile.Droid.Fragment
+{
+    public class BottomSheetExample : BottomSheetDialogFragment
+    {
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            base.OnCreateView(inflater, container, savedInstanceState);
+            return inflater.Inflate(Resource.Layout.bottom_sheet_layout, container, false);
+        }
+
+    }
+}
---
+++ Sample/Fragments/HomeFragment.cs
@@ -0,0 +1,48 @@
+﻿using Android.OS;
+using Android.Views;
+using Android.Widget;
+using AndroidX.Fragment.App;
+using AndroidX.Navigation;
+
+namespace com.companyname.NavigationCodeLab.Fragments
+{
+    public class HomeFragment : Fragment //, View.IOnClickListener
+    {
+        public HomeFragment() { }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            base.OnCreateView(inflater, container, savedInstanceState);
+            
+            HasOptionsMenu = true;
+            return inflater.Inflate(Resource.Layout.home_fragment, container, false);
+        }
+
+        public override void OnCreateOptionsMenu(IMenu menu, MenuInflater inflater)
+        {
+            base.OnCreateOptionsMenu(menu, inflater);
+            inflater.Inflate(Resource.Menu.main_menu, menu);
+        }
+
+        public override void OnViewCreated(View view, Bundle savedInstanceState)
+        {
+            base.OnViewCreated(view, savedInstanceState);
+
+            NavOptions navOptions = new NavOptions.Builder()
+                .SetEnterAnim(Resource.Animation.slide_in_right) 
+                .SetExitAnim(Resource.Animation.slide_out_left) 
+                .SetPopEnterAnim(Resource.Animation.slide_in_left)
+                .SetPopExitAnim(Resource.Animation.slide_out_right)
+                .Build();
+
+            view.FindViewById<Button>(Resource.Id.navigate_destination_button).Click += (sender, e) =>
+            {
+                Navigation.FindNavController((View)sender).Navigate(Resource.Id.bottom_sheet_dest, null, navOptions);
+            };
+        }
+        public void OnClick(View v)
+        {
+            Navigation.FindNavController(v).Navigate(Resource.Id.flow_step_one_dest, null);
+        }
+    }
+}
